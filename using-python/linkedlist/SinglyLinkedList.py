79422bcb796a94505073b9545302430408ec0e86
sanalkumars <sanalskumar126@gmail.com>
2026-01-26 10:03:42 +0530

    insert at any where done

--- using-python/linkedlist/SinglyLinkedList.py
+++ using-python/linkedlist/SinglyLinkedList.py
@@ -6,7 +6,8 @@ class Node:
 class SinglyLinkedList:
     def __init__(self,head=None):
         self.head = head
-    
+   
+
     # insert at end
     def insertEnd(self,value):
         # we create a new node using the Node class, also asign it to a new variable
@@ -27,6 +28,24 @@ class SinglyLinkedList:
         temp = self.head
         self.head = newnode
         newnode.next = temp
+    
+    def inserAtAnywhere(self,value,pos):
+        newnode = Node(value)
+        if pos == 0 or self.head is None:
+            newnode.next = self.head
+            self.head= newnode
+            return
+        current = self.head
+        count = 0
+        while count < pos-1 and current.next is not None:
+            current = current.next
+            count +=1
+
+        if count != pos - 1:
+            print(f"Position {pos} is out of bounds")
+            return
+        newnode.next = current.next
+        current.next= newnode
 
     def printLinkedList(self):
         if self.head is None:
@@ -40,9 +59,13 @@ class SinglyLinkedList:
 
 list = SinglyLinkedList()
 
+list.insertEnd(20)
 list.insertEnd(30)
 list.insertEnd(40)
 list.insertEnd(50)
 
+# list.insertStart(20)
+
+list.inserAtAnywhere(60,3)
 list.printLinkedList()
         
