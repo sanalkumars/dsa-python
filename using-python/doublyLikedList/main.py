fbfc88eea2137898947da0cbddfea3ed71661b67
sanalkumars <sanalskumar126@gmail.com>
2026-02-09 11:48:11 +0530

    insert at begining , insert at any where is done

--- using-python/doublyLikedList/main.py
+++ using-python/doublyLikedList/main.py
@@ -21,13 +21,58 @@ class DoublyLinkedList:
         temp.next = newNode
         newNode.prev = temp
 
+    def InsertAtBegining(self , value):
+        newnode = Node(value)
+
+        if( self.head is None):
+            self.head = newnode
+            return
+        newnode.next = self.head
+        self.head.prev
+        self.head = newnode
+
+    def InsertAtAnywhere( self , value , pos):
+        newnode = Node(value)
+        # condition 0 is the list is empty
+        if self.head is None:
+            print("the list is empty so the value is insert at the begining")
+            self.head = newnode
+            return
+        # condition 1 selected position is the first position 
+        if pos == 1:
+            newnode.next = self.head
+            if self.head  is not None:
+                self.head.prev = newnode
+            self.head = newnode
+            return
+        
+        count = 1
+        temp = self.head
+        prev = None
+        while( temp is not None and count < pos ):
+            prev = temp
+            temp = temp.next
+            count +=1
+
+        if temp is None and count < pos :
+            print(f"position {pos} is out of bound ")
+            return
+        
+        prev.next = newnode
+        newnode.prev = prev
+        newnode.next = temp
+        if temp is not None:
+            temp.prev = newnode
+        
+
+
     def printLinkedList(self):
         if self.head is None:
             return
         else:
             current = self.head
             while current is not None:
-                print("<=>",current.data,"<=>")
+                print(current.data,end="<=>")
                 current=current.next
 
 # creating a doubly lisked list using the class
@@ -38,5 +83,10 @@ list.InsertNewNodeAtEnd(10)
 list.InsertNewNodeAtEnd(20)
 list.InsertNewNodeAtEnd(30)
 list.InsertNewNodeAtEnd(40)
+
+list.InsertAtBegining(9)
+
+list.InsertAtAnywhere(8,3)
+
 
 list.printLinkedList()
