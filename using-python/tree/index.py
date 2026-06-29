c8b29a313b3a8a2a9212e2dfa755fa6c6b2cfe6f
sanalkumars <sanalskumar126@gmail.com>
2026-06-29 11:44:51 +0530

    implement preorder and inorder traversal methods, and fix BFS implementation in the Tree class

--- using-python/tree/index.py
+++ using-python/tree/index.py
@@ -34,5 +34,58 @@ class Tree :
                 else:
                     current = current.right
 
+    def preOrderTraversal(self):
+        self._preorderHelper(self.root)
+        print()
+        
+    def _preorderHelper(self, node):
+        if node is None:
+            return
+        print(node.val,end=" ") #visited the node
+        self._preorderHelper( node.left) #visit the left side after root
+        self._preorderHelper(node.right) # visit the right side after root
+
+    def inorderTraversal(self):
+        self._inorderHelper(self.root)
+    
+    def _inorderHelper(self , node):
+        if node is None:
+            return
+        self._inorderHelper(node.left)
+        print(node.val,end=" ")
+        self._inorderHelper(node.right)
+
+    # level by search or BFS
+    # for this we need a doubly ended queue
+    from collections import deque
+    def breathFirstSearch(self):
+        if not self.root:
+            return
+        queue = self.deque([self.root])
+        while queue:
+            node = queue.popleft()
+            print(node.val,end=" ")
+            if node.left :
+                queue.append(node.left)
+            if node.right :
+                queue.append(node.right)
+
+
+        
+
+mytree = Tree()
+mytree.insertNode(10)
+mytree.insertNode(1)
+mytree.insertNode(5)
+mytree.insertNode(11)
+mytree.insertNode(112)
+
+# mytree.preOrderTraversal()
+
+# mytree.inorderTraversal()
+
+
+mytree.breathFirstSearch()
+
 
         
